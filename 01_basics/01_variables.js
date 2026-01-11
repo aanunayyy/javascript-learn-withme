29b8fe2175e3bacb4b96e31882b4b63cb0ab84f3
Anunay Pandey <anunaypandey115@gmail.com>
2026-01-11 15:06:32 +0000

    variables and datatypes notes added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 14453
+let accountEmail = "anunaypandey115@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+//accountId = 2 // not allowed
+accountEmail = "anunay.pandey.cs28"
+accountPassword = "21212121"
+accountCity = "1234r"
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treats all JS code as newer version
+//alert(3+3) // we are using nodejs, not browser
+let name = "anunay"
+let age = 19
+let state;
+
+//number =>  2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone Value
+//undefined => 
+// symbol => unique
+//object 
+
+console.log(typeof "anunay");
+console.log(typeof null);
+console.log(typeof undefined);
