3f99bdaf258b17f9267e00a4a9a3df9c3dcfa5f3
Anunay Pandey <anunaypandey115@gmail.com>
2026-01-15 13:39:50 +0000

    changed the conversionOperation file

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,1 +1,12 @@
 let score = 33 
+//console.log(type of score);
+//console.log(type of(score));
+
+//"33" + 33
+/* let isLoggedIn = ""  false
+ let isLoggedIn = "anunay"  true
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+ */
+//*********************************** */
+//Operations
